23ed66519600f4b279859f063a00bff81ea3f720
Juan Carlos Vidal Domínguez <juansinmiedos@MacBook-Pro-de-Juan-3.local>
2026-03-16 09:46:15 -0600

    delete json document, all fields move to .env

--- controllers/form.js
+++ controllers/form.js
@@ -1,9 +1,18 @@
 import { google } from "googleapis"
-import fs from "fs"
 
-const credentials = JSON.parse(fs.readFileSync("google-sheets-key.json", "utf8"))
-credentials.private_key_id = process.env.GOOGLE_PRIVATE_KEY_ID
-credentials.private_key = process.env.GOOGLE_PRIVATE_KEY
+const credentials = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY_ID,
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URL,
+  token_uri: process.env.GOOGLE_TOKEN_URL,
+  auth_provider_x509_cert_url: process.env.GOOGLE_PROVIDER_CERT,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT,
+  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+}
 
 const auth = new google.auth.GoogleAuth({
   credentials,
